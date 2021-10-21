012706fb08cf9a95c4682ae094790fda4c9ec998
Ryan Bibby <ryan.bibby@ukfast.co.uk>
2021-10-21 08:38:58 +0100

    Update API key, remove issues section

--- script.js
+++ script.js
@@ -2,19 +2,10 @@ $(document).ready(function() {
 	var config = {
 		uptimerobot: {
 			api_keys: [
+				'ur491309-36117bc8f6a6bf6d221a0167'
 			],
 			logs: 1
-		},
-		github: {
-			org: 'flybaseio',
-			repo: 'status'
 		}
-	};
-	var status_text = {
-		'operational': 'operational',
-		'investigating': 'investigating',
-		'major outage': 'outage',
-		'degraded performance': 'degraded',
 	};
 
 	var monitors = config.uptimerobot.api_keys;
@@ -61,81 +52,5 @@ $(document).ready(function() {
 								'<h4 class="list-group-item-heading">' + name + '</h4>' +
 								'</div>');
 		});
-	};
-
-	$.getJSON( 'https://api.github.com/repos/' + config.github.org + '/' + config.github.repo + '/issues?state=all' ).done(message);
-
-	function message(issues) {
-		issues.forEach(function(issue) {
-			if( issue.labels.length > 0 ){ // only display labelled issues
-				var status = issue.labels.reduce(function(status, label) {
-					if (/^status:/.test(label.name)) {
-						return label.name.replace('status:', '');
-					} else {
-						return status;
-					}
-				}, 'operational');
-
-				var systems = issue.labels.filter(function(label) {
-					return /^system:/.test(label.name);
-				}).map(function(label) {
-					return label.name.replace('system:', '')
-				});
-
-				if (issue.state === 'open') {
-					$('#panel').data('incident', 'true');
-					$('#panel').attr('class', (status === 'operational' ? 'panel-success' : 'panel-warn') );
-					$('#paneltitle').html('<a href="#incidents">' + issue.title + '</a>');
-				}
-
-				var html = '<article class="timeline-entry">\n';
-				html += '<div class="timeline-entry-inner">\n';
-
-				if (issue.state === 'closed') {
-					html += '<div class="timeline-icon bg-success"><i class="entypo-feather"></i></div>';
-				} else {
-					html += '<div class="timeline-icon bg-secondary"><i class="entypo-feather"></i></div>';
-				}
-
-				html += '<div class="timeline-label">\n';
-				html += '<span class="date">' + datetime(issue.created_at) + '</span>\n';
-
-				// status
-				if (issue.state === 'closed') {
-					html += '<span class="badge label-success pull-right">closed</span>';
-				} else {
-					html += '<span class="badge ' + (status === 'operational' ? 'label-success' : 'label-warn') + ' pull-right">';
-					html += "open";
-					html += '</span>\n';
-				}
-
-				// systems
-				for (var i = 0; i < systems.length; i++) {
-					html += '<span class="badge system pull-right">' + systems[i] + '</span>';
-				}
-
-				html += '<h2>' + issue.title + '</h2>\n';
-				html += '<hr>\n';
-				html += '<p>' + issue.body + '</p>\n';
-
-				if (issue.state === 'closed') {
-					html += '<p><em>Updated ' + datetime(issue.closed_at) + '<br/>';
-					html += 'The system is back in normal operation.</p>';
-				}
-				html += '</div>';
-				html += '</div>';
-				html += '</article>';
-				$('#incidents').append(html);
-			}
-		});
-
-		function datetime(string) {
-			var datetime = string.split('T');
-
-			var date = datetime[0];
-			var time = datetime[1].replace('Z', '');
-
-			return date + ' ' + time;
-		};
 	};
 });
